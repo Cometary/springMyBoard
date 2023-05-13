27ca250ca891eb2736d793eeeb5502544f198120
Cometary <109892844+Cometary@users.noreply.github.com>
2023-05-14 08:40:58 +0900

    wq

--- myboard/src/main/java/tommy/spring/web/common/AroundAdvice.java
+++ myboard/src/main/java/tommy/spring/web/common/AroundAdvice.java
@@ -1,12 +1,23 @@
 package tommy.spring.web.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 
+@Service
+@Aspect
 public class AroundAdvice {
+	@Around("PointcutCommon.allPointcut()")
 	public Object aroundLog(ProceedingJoinPoint joinPoint) throws Throwable {
-		System.out.println("[BEFORE] : 비즈니스 메서드 수행 전에 처리할 내용 ...");
+		String method = joinPoint.getSignature().getName();
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
 		Object returnObj = joinPoint.proceed();
-		System.out.println("[AFTER] 비즈니스 메서드 수행 후에 처리할 내용 ...");
+		stopWatch.stop();
+		System.out.println(method + "() 메서드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)초");
 		return returnObj;
 	}
+
 }
