3c688138d8de47dadf2a7ff56c7610468c9ba86f
Cometary <109892844+Cometary@users.noreply.github.com>
2023-06-12 08:53:09 +0900

    asd

--- myboard/src/main/java/tommy/spring/web/board/BoardVO.java
+++ myboard/src/main/java/tommy/spring/web/board/BoardVO.java
@@ -2,6 +2,13 @@ package tommy.spring.web.board;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -11,19 +18,27 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@Entity
+@Table(name = "MYBOARD")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class BoardVO {
+	@Id
+	@GeneratedValue
 	@XmlAttribute
 	private int seq;
 	private String title;
 	private String writer;
 	private String content;
-	private Date regDate;
+	@Temporal(TemporalType.DATE)
+	private Date regDate = new Date();
 	private int cnt;
+	@Transient
 	@XmlTransient
 	private String searchCondition;
+	@Transient
 	@XmlTransient
 	private String searchKeyword;
+	@Transient
 	@XmlTransient
 	private MultipartFile uploadFile;
 
